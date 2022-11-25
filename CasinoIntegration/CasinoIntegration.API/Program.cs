31fb5c256cdb00cc6d34ce5f9df67c63207bf36f
mershalova <angelamersh@gmail.com>
2022-11-25 13:15:15 +0200

    adding scoped services

--- CasinoIntegration/CasinoIntegration.API/Program.cs
+++ CasinoIntegration/CasinoIntegration.API/Program.cs
@@ -1,6 +1,20 @@
+using CasinoIntegration.BusinessLayer.CasinoInegration.Services.Interfaces;
+using CasinoIntegration.BusinessLayer.CasinoInegration.Services;
+using Microsoft.Extensions.Options;
+using CasinoIntegration.DataAccessLayer.CasinoIntegration.DatabaseSettings;
+using CasinoIntegration.DataAccessLayer.CasinoIntegration.DatabaseSettings.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<CasinoIntegrationDatabaseSettings>(
+    builder.Configuration.GetSection(nameof(CasinoIntegrationDatabaseSettings)));
+
+builder.Services.AddSingleton<ICasinoIntegrationDatabaseSettings>(provider =>
+    provider.GetRequiredService<IOptions<CasinoIntegrationDatabaseSettings>>().Value);
+
+builder.Services.AddScoped<IPlayerService, PlayerService>();
+builder.Services.AddScoped<IMachineService, MachineService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
